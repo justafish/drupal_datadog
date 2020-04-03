6a53f45ab8685b18a44990242532828e44f1e4f9
Sally Young <sally@justafish.co.uk>
2020-04-03 18:18:18 +0100

    (bug) Allow the service name to be overridden

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Overrides the data format used by [Logs HTTP](https://www.drupal.org/project/logs_http)
 to be compatible with DataDog's log intake endpoint.
+
+You can set your service name with the Logs HTTP UI, or override it by setting
+`$settings['datadog_service'] = 'drupal-local';` in `settings.php`
+(See https://github.com/Gizra/logs_http/issues/24)
--- src/DatadogLogsHttpLogger.php
+++ src/DatadogLogsHttpLogger.php
@@ -3,58 +3,67 @@
 namespace Drupal\Datadog;
 
 use Drupal\logs_http\Logger\LogsHttpLogger;
+use Drupal\Core\Site\Settings;
 
 class DatadogLogsHttpLogger extends LogsHttpLogger {
 
-  /**
-   * {@inheritdoc}
-   */
-  public function registerEvent($level, $message, array $context = []) {
-    if (!$this->isEnabled()) {
-      return;
+    /**
+     * {@inheritdoc}
+     */
+    public function registerEvent($level, $message, array $context = []) {
+        if (!$this->isEnabled()) {
+            return;
+        }
+
+        // Populate the message placeholders and then replace them in the message.
+        $message_placeholders = $this->logMessageParser->parseMessagePlaceholders($message, $context);
+        $message = empty($message_placeholders) ? $message : strtr($message, $message_placeholders);
+        $referer = parse_url($context['referer']);
+
+        $event = [
+            'source' => 'php',
+            'level' => $level,
+            'level_name' => $this->severityLevels[$level]->getUntranslatedString(),
+            'host' => isset($referer['host']) ? $referer['host'] : NULL,
+            'http' => [
+                'url' => $context['request_uri'],
+                'referer' => $context['referer'],
+            ],
+            'network' => [
+                'ip' => $context['ip'],
+            ],
+            'message' => $message,
+            'context' => [
+                'user' => $context['uid'],
+                'timestamp' => $context['timestamp'],
+            ],
+        ];
+
+        if (!empty($context['exception_trace'])) {
+            // We avoid unserializing as it seems to causes Logs to fail to index
+            // event as JSON.
+            $event['exception_trace'] = base64_decode($context['exception_trace']);
+        }
+
+        $uuid = $this->config->get('uuid');
+        $service = settings::get('datadog_service', NULL);
+        if (!empty($uuid)) {
+            $event['service'] = $uuid;
+        }
+
+        // Allow the above to be overriden by settings because you can't override a
+        // config key named "uuid".
+        if (!empty($service)) {
+            $event['service'] = $service;
+        }
+
+        // Remove empty values, to prevent errors in the indexing of the JSON.
+        $event = $this->arrayRemoveEmpty($event);
+
+        // Prevent identical events.
+        $event_clone = $event;
+        unset($event_clone['timestamp']);
+        $key = md5(serialize($event_clone));
+        $this->cache[$key] = $event;
     }
-
-    // Populate the message placeholders and then replace them in the message.
-    $message_placeholders = $this->logMessageParser->parseMessagePlaceholders($message, $context);
-    $message = empty($message_placeholders) ? $message : strtr($message, $message_placeholders);
-    $referer = parse_url($context['referer']);
-
-    $event = [
-      'source' => 'php',
-      'level' => $level,
-      'level_name' => $this->severityLevels[$level]->getUntranslatedString(),
-      'host' => isset($referer['host']) ? $referer['host'] : NULL,
-      'http' => [
-        'url' => $context['request_uri'],
-        'referer' => $context['referer'],
-      ],
-      'network' => [
-        'ip' => $context['ip'],
-      ],
-      'message' => $message,
-      'context' => [
-        'user' => $context['uid'],
-        'timestamp' => $context['timestamp'],
-      ],
-    ];
-
-    if (!empty($context['exception_trace'])) {
-      // We avoid unserializing as it seems to causes Logs to fail to index
-      // event as JSON.
-      $event['exception_trace'] = base64_decode($context['exception_trace']);
-    }
-
-    if ($uuid = $this->config->get('uuid')) {
-      $event['service'] = $uuid;
-    }
-
-    // Remove empty values, to prevent errors in the indexing of the JSON.
-    $event = $this->arrayRemoveEmpty($event);
-
-    // Prevent identical events.
-    $event_clone = $event;
-    unset($event_clone['timestamp']);
-    $key = md5(serialize($event_clone));
-    $this->cache[$key] = $event;
-  }
 }
